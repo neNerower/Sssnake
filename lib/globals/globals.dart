6c24c54035e7a652678a1ee5bcb2c7611f969d95
Nerower <p0sirega121234@gmail.com>
2021-09-08 04:03:06 +0300

    lib: snake model created

---
+++ lib/globals/globals.dart
@@ -0,0 +1,5 @@
+library globals;
+
+// Game field size in sells amount
+int fieldHeight = 35;
+int fieldWidth = 20;
---
+++ lib/models/snake/snake_model.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+import 'package:snake/globals/globals.dart';
+import 'direction.dart';
+
+class SnakeModel {
+  List<int> _body = [];
+  late Direction _moveDirection;
+
+  SnakeModel.getSnake({int length = 5}) {
+    Random random = new Random();
+
+    // Get random direction to start moving
+    int index = random.nextInt(Direction.values.length);
+    this._moveDirection = Direction.values[index];
+
+    // Get random start head position
+    int hCoeff = length + random.nextInt(fieldWidth - length);
+    int vCoeff = length + random.nextInt(fieldHeight - length);
+    int headPosition = hCoeff * vCoeff;
+
+    // Fill snake body
+    int bodyCoeff = _getBodyCoeff(this._moveDirection);
+    this._body = List.generate(length, (int i) => headPosition + i*bodyCoeff);
+  }
+
+  // Count coefficient to fill snake body list
+  int _getBodyCoeff(Direction direction) {
+    switch (direction) {
+      case Direction.up: return fieldWidth;
+      case Direction.down: return -fieldWidth;
+      case Direction.left: return 1;
+      case Direction.right: return -1;
+    }
+  }
+
+  
+}
