f5ae693dc1711afed412fae4871bd4d54995142b
Nerower <p0sirega121234@gmail.com>
2021-09-10 15:27:07 +0300

    lib: models: snake_model refact

--- lib/models/snake/snake_model.dart
+++ lib/models/snake/snake_model.dart
@@ -4,14 +4,14 @@ import 'direction.dart';
 
 class SnakeModel {
   List<int> _body = [];
-  late Direction _moveDirection;
+  late Direction _direction;
 
-  SnakeModel.getSnake({int length = 5}) {
+  SnakeModel({int length = 5}) {
     Random random = new Random();
 
     // Get random direction to start moving
     int index = random.nextInt(Direction.values.length);
-    this._moveDirection = Direction.values[index];
+    this._direction = Direction.values[index];
 
     // Get random start head position
     int hCoeff = length + random.nextInt(fieldWidth - length);
@@ -19,19 +19,28 @@ class SnakeModel {
     int headPosition = hCoeff * vCoeff;
 
     // Fill snake body
-    int bodyCoeff = _getBodyCoeff(this._moveDirection);
-    this._body = List.generate(length, (int i) => headPosition + i*bodyCoeff);
+    int bodyCoeff = _getBodyCoeff(this._direction);
+    this._body = List.generate(length, (int i) => headPosition + i * bodyCoeff);
   }
 
   // Count coefficient to fill snake body list
   int _getBodyCoeff(Direction direction) {
     switch (direction) {
-      case Direction.up: return fieldWidth;
-      case Direction.down: return -fieldWidth;
-      case Direction.left: return 1;
-      case Direction.right: return -1;
+      case Direction.up:
+        return fieldWidth;
+      case Direction.down:
+        return -fieldWidth;
+      case Direction.left:
+        return 1;
+      case Direction.right:
+        return -1;
     }
   }
 
-  
+  Direction getDirection() {
+    return this._direction;
+  }
+  void setDirection(Direction direction) {
+    this._direction = direction;
+  }
 }
