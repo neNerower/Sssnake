7b0290a153459defcfd3736f6da9758f36b311c3
Nerower <p0sirega121234@gmail.com>
2021-09-10 19:33:24 +0300

    lib: screens: cell component created

---
+++ lib/screens/components/cell.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Cell extends StatelessWidget {
+  final Color color;
+  const Cell({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(2),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Container(color: color),
+        ),
+      ),
+    );
+  }
+}
