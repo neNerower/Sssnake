8a9e7499bc87340be0edaea3540ec80e94710b6c
Nerower <p0sirega121234@gmail.com>
2021-09-15 16:50:24 +0300

    lib: game_field extracted

--- lib/screens/game_screen/game_screen.dart
+++ lib/screens/game_screen/game_screen.dart
@@ -3,9 +3,9 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:snake/globals/globals.dart';
 import 'package:snake/models/field/field_model.dart';
-import 'package:snake/screens/game_screen/play_button.dart';
-
-import 'game_field/game_field.dart';
+import 'package:snake/models/snake/direction.dart';
+import 'package:snake/screens/game_screen/game_field/game_field.dart';
+import 'package:snake/screens/game_screen/game_field/play_button.dart';
 
 class GameScreen extends StatefulWidget {
   GameScreen({Key? key}) : super(key: key);
@@ -47,7 +47,33 @@ class _GameScreenState extends State<GameScreen> {
         backgroundColor: Colors.black,
         body: Column(
           children: <Widget>[
-            GameField(fieldModel: _fieldModel),
+            Expanded(
+              child: GestureDetector(
+                onVerticalDragUpdate: (details) {
+                  if (_fieldModel.getSnake().getDirection() != Direction.down &&
+                      details.delta.dy < 0) {
+                    // Turn UP
+                    _fieldModel.getSnake().setDirection(Direction.up);
+                  } else if (_fieldModel.getSnake().getDirection() != Direction.up &&
+                      details.delta.dy > 0) {
+                    // Turn DOWN
+                    _fieldModel.getSnake().setDirection(Direction.down);
+                  }
+                },
+                onHorizontalDragUpdate: (details) {
+                  if (_fieldModel.getSnake().getDirection() != Direction.right &&
+                      details.delta.dx < 0) {
+                    // Turn LEFT
+                    _fieldModel.getSnake().setDirection(Direction.left);
+                  } else if (_fieldModel.getSnake().getDirection() != Direction.left &&
+                      details.delta.dx > 0) {
+                    // Turn RIGHT
+                    _fieldModel.getSnake().setDirection(Direction.right);
+                  }
+                },
+                child: GameField(fieldModel: _fieldModel),
+              ),
+            )
           ],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
